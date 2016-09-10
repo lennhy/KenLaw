8df83fa000f94ec189a8ed66c20e41d94d0898d2
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-10 06:36:11 +0000

    Appliedd helper methods and html to index view

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,18 +1,17 @@
 require 'sinatra/base'
 class Helpers
-# there is information on most pages that are dependent on being logged in. You can see a lot of information if you are logged in, and practically none if you're not. This sort of application flow is handled without logic by the use of helper methods.
-
-# Instead of writing that type of logic directly in your view, we use helper methods. Helper methods are methods that are written in your controller, that are accessible in your views, and provide some support.
-
- helpers do
-
-    def current_user
+    # --To add double protection to this view
+    def self.current_user(session)
       User.find(session[:user_id])
     end
+
+    def self.is_logged_in?(session)
+      !!session[:user_id]
+    end
+
   end
 
-  def self.is_logged_in?
-    !!session[:user_id]
-  end
-  
 end
+# --there is information on most pages that are dependent on being logged in. You can see a lot of information if you are logged in, and practically none if you're not. This sort of application flow is handled without logic by the use of helper methods.
+
+# --Instead of writing that type of logic directly in your view, we use helper methods. Helper methods are methods that are written in your controller, that are accessible in your views, and provide some support.
