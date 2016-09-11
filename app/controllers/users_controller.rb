7ad4eee04e59c3df03f4d9e57ae45191f9bc36bf
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-11 23:35:35 +0000

    added bootstrap css elements, added logic to logout get route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  # -- takes you to the second page for new users signup
+  # -- takes you to the signup page from the home page for new users
   get "/signup" do
     if !session[:user_id] # -- if user is not logged in
      erb :"users/create_user"
@@ -22,9 +22,13 @@ class UsersController < ApplicationController
     end
   end
 
-  # -- login page
+  # -- get to the login page view
   get "/login" do
+    if session[:user_id].nil?
       erb :"users/login"
+    else
+      redirect "/users_questions"
+    end
   end
 
   # -- after attempt to login
@@ -38,8 +42,12 @@ class UsersController < ApplicationController
   end
 
   get "/logout" do
-    session.destroy
-    redirect to "/login"
+    if !session[:user_id].nil?
+      session.destroy
+      redirect to "/login"
+    else
+      redirect to "/"
+    end
   end
 
 end
