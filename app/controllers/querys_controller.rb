f1711ce55bfa0af0e9f238eff27f80037f79b533
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-11 01:32:56 +0000

    ran rake db:seed

--- app/controllers/querys_controller.rb
+++
@@ -1,2 +0,0 @@
-class QuerysController < ApplicationController
-end
--- app/models/query.rb
+++
@@ -1,8 +0,0 @@
-class Query < ActiveRecord::Base
-  has_many :results
-  belongs_to :users
-
-  # extend Slugifiable::ClassMethods
-  # include Slugifiable::InstanceMethods
-
-end
