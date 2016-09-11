c1c80de0cc2c16214ab5dddbacc81f70f8a5d807
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-11 18:45:30 +0000

    removed results page, this page is not necessary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,33 @@
 class UsersController < ApplicationController
 
-# -- takes you to the second page for new users signup
+  # -- takes you to the second page for new users signup
   get "/signup" do
      erb :"users/create_user"
   end
 
+  # -- after attempt to signup
+  post "/signup" do
+    redirect to "/users_questions"
+  end
+
+  # -- login page
   get "/login" do
       erb :"users/login"
   end
 
-# -- after attempt to login
+  # -- after attempt to login
   post "/login" do
     redirect to "/users_questions"
   end
 
   # -- show users page with all their posted questions
-  get "/show"
-    erb :"/show"
+  get "/show" do
+    erb :"users/show"
   end
+
+  get "/logout" do
+    session.destroy
+    redirect to "/login"
+  end
+
 end
