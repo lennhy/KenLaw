edeab19d0d1f5b78164d857cc83ef5aa80de4a2b
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-14 16:39:33 +0000

    Added user show page and controller action , added show page for amendments and controller action. Added links to each show page by id.

--- app/controllers/amendments_controller.rb
+++ app/controllers/amendments_controller.rb
@@ -2,18 +2,12 @@ class AmendmentsController < ApplicationController
   # Amendments route controller
     get '/amendments' do
       @amendments = Amendments.all
-      erb :'amendments/show_amendments'
+      erb :'amendments/amendments'
     end
 
-    # get '/genres/show' do
-    #   erb :'genres/show'
-    # end
-    #
+    # -- show individual amendment 
     get'/amendments/:id' do
-      @user = User.find_by_slug(params[:id])
-      @question = Question.find_by_id(params[:id])
       @amendment = Amendment.find_by_id(params[:id])
-
       erb :'amendments/show_amendment'
     end
 
