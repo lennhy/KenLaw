beba2efc84a64040a260978cb9e6ef5c2e6f18bf
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-14 03:39:04 +0000

    added patch, delete controller actions, created associated forms. PS sql search query only works if user enters one word in search field.

--- app/controllers/amendments_controller.rb
+++ app/controllers/amendments_controller.rb
@@ -1,2 +1,21 @@
 class AmendmentsController < ApplicationController
+  # Amendments route controller
+    get '/amendments' do
+      @amendments = Amendments.all
+      erb :'amendments/show_amendments'
+    end
+
+    # get '/genres/show' do
+    #   erb :'genres/show'
+    # end
+    #
+    get'/amendments/:id' do
+      @user = User.find_by_slug(params[:id])
+      @question = Question.find_by_id(params[:id])
+      @amendment = Amendment.find_by_id(params[:id])
+
+      erb :'amendments/show_amendment'
+    end
+
+
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,8 @@ class QuestionsController < ApplicationController
     end
   end
 
-  get '/questions/create_question' do
+  # -- get the create a question page
+  get '/users_questions/create_question' do
     if session[:user_id]
        erb :"questions/create_question"
      else
@@ -21,26 +22,76 @@ class QuestionsController < ApplicationController
   # -- see the questions of the individual user
   post '/users_questions' do
     if params[:content] == "" # --check if the user entered correct information if not redirect
-      redirect to "/questions/create_question"
+      redirect to "/users_questions/create_question"
     else
       user = User.find_by_id(session[:user_id])
       @question = Question.create(:content => params[:content], :user_id => user.id)
       @search = params[:content]
-      @amendment = Amendment.find_by_sql("SELECT * FROM Amendments WHERE Content LIKE '%#{@search}%';")
+      # -- find the closest matching amendment for the search entered in the create a question page
+      @amendment = Amendment.find_by_sql("SELECT * FROM Amendments WHERE Name OR Content LIKE '%#{@search}%'")
+      # -- associate the amendment that was returned to belong to the question
       @question.amendments << @amendment
-      redirect to "/questions/#{@question.id}"
+      @question.save
+      redirect to "/users_questions/#{@question.id}"
     end
-end
-
-get '/questions/:id' do
-  if session[:user_id]
-    @question = Question.find_by_id(params[:id])
-    erb :'questions/show_question'
-  else
-    redirect to '/login'
   end
-end
 
+  # -- see the newly created question and its corresponding amendment result from the search
+  get '/users_questions/:id' do
+    if session[:user_id]
+      @question = Question.find_by_id(params[:id])
+      erb :'questions/show_question'
+    else
+      redirect to '/login'
+    end
+  end
+
+  # -- change question
+  get '/users_questions/:id/edit' do
+    if session[:user_id]
+      @question = Question.find_by_id(params[:id])
+      if @question.user_id == session[:user_id]
+       erb :'questions/edit_question'
+      else
+        redirect to '/users_questions'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  # --edit question and return a new amendment result
+  patch '/users_questions/:id' do
+    if params[:content] == ""
+      redirect to "/questions/#{params[:id]}/edit"
+    else
+      @question = Question.find_by_id(params[:id])
+      @question.amendments.clear
+      @question.content = params[:content]
+      @search = params[:content]
+      @amendment = Amendment.find_by_sql("SELECT * FROM Amendments WHERE Name OR Content LIKE '%#{@search}%'")
+      @question.amendments << @amendment
+      @question.save
+      redirect to "/users_questions/#{@question.id}"
+    end
+  end
+
+  # --delete question and corresponding amendment result
+  delete '/users_questions/:id/delete' do
+    @question = Question.find_by_id(params[:id])
+    if session[:user_id]
+      @question = Question.find_by_id(params[:id])
+      if @question.user_id == session[:user_id]
+        @question.delete
+        @question.amendments.delete
+        redirect to '/users_questions'
+      else
+        redirect to '/users_questions'
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
 
 end
