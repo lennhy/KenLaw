7e353915a7f82f51dec85f46ee21cbb196cea6a2
lenn hypolite <lennhydesign@gmail.com>
2017-01-11 21:02:36 -0500

    Successfully created a custom filter for the DOM on create question page

--- public/assets/javascript/code.js
+++ public/assets/javascript/code.js
@@ -1,24 +1,35 @@
-var liNodes = document.querySelectorAll('ul.amendments li');
-// var searchText =
+var liNodes = document.querySelectorAll('ol.amendments li');
+var inputText = document.getElementById("searchBox");
+var search = "";
+
 
 function Amendments(elementNodes, searchText) {
   this.textNodes = elementNodes;
-}
-
-//  filter function
-function checkSearchText(text){
-    return text === textNodeWord;
+  this.searchText = searchText;
 }
 
 Amendments.prototype.filterAmendments = function(){
+  var regex = new RegExp(this.searchText);
+  console.log(regex);
+
   for(let i=0; i < this.textNodes.length; i++){
     var textNode = this.textNodes[i].innerHTML;
-    console.log(textNode);
+    cleanTextNode = textNode.replace(/;/g, " ");
+    if(regex.test(cleanTextNode)){
+      console.log(textNode);
+    }
   }
+  
 }
 
-// create a new instance of the amendments constructor
-var filteredAmendments = new Amendments(liNodes);
+// Dynamically update the DOM or console every time user enters a key
+inputText.addEventListener('keyup', function() {
+  // save value that user types in search box
+  search = inputText.value;
 
-// call the prototype function on the constructor instance
-filteredAmendments.filterAmendments();
+  // create a new instance of the amendments constructor
+  var filteredAmendments = new Amendments(liNodes, search);
+
+   filteredAmendments.filterAmendments();
+
+}, false);
