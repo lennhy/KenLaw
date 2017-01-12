0dc43743b9a92d44f1011352a8a2f91470fde830
lenn hypolite <lennhydesign@gmail.com>
2017-01-11 22:50:58 -0500

    Encapsulated the code to run in a window.onload function

--- public/assets/javascript/code.js
+++ public/assets/javascript/code.js
@@ -1,37 +1,40 @@
 var liNodes = document.querySelectorAll('ol.amendments li');
 var inputText = document.getElementById("searchBox");
-var search = "";
 
 
 function Amendments(elementNodes, searchText) {
   this.textNodes = elementNodes;
-  this.searchText = searchText;
+  this.searchText = searchText.toLowerCase();
 }
 
 Amendments.prototype.filterAmendments = function(){
-  var regex = new RegExp(this.searchText);
+  let regex = new RegExp(this.searchText);
 
-  var list = document.getElementsByClassName('amendments');
-  var result = [];
+  let list = document.getElementsByClassName('amendments');
+
+  let result = [];
+
   for(let i=0; i < this.textNodes.length; i++){
-    var textNode = this.textNodes[i].innerHTML;
-    var cleanTextNode = textNode.replace(/;/g, " ");
+    let textNode = this.textNodes[i].innerHTML;
+    let cleanTextNode = textNode.replace(/;/g, " ").toLowerCase();
 
     if(regex.test(cleanTextNode)){
        result.push("<li>"+ textNode + "</li>");
     }
   }
-list[0].innerHTML = result;
+  list[0].innerHTML = result;
 }
 
-// Dynamically update the DOM or console every time user enters a key
-inputText.addEventListener('keyup', function() {
-  // save value that user types in search box
-  search = inputText.value;
-
-  // create a new instance of the amendments constructor
-  var filteredAmendments = new Amendments(liNodes, search);
-
-   filteredAmendments.filterAmendments();
-
-}, false);
+// if there is something to return start the eventListener
+window.onload = function runCode(){
+  if (inputText !== null) {
+    // Dynamically update the DOM or console every time user enters a key
+    inputText.addEventListener('keyup', function() {
+      // save value that user types in search box
+      var search = inputText.value;
+      // create a new instance of the amendments constructor
+      var filteredAmendments = new Amendments(liNodes, search);
+       filteredAmendments.filterAmendments();
+    }, false);
+  }
+}
