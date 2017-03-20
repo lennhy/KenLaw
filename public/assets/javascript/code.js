583de0cced8b234550a258068173a8573fc16a31
lenn hypolite <lennhydesign@gmail.com>
2017-03-20 10:23:11 -0400

    Getting same bug with old code

--- public/assets/javascript/code.js
+++ public/assets/javascript/code.js
@@ -1,65 +1,6 @@
-// var liNodes = document.querySelectorAll('.amendments ul.list-group li');
-// var inputText = document.getElementById("searchBox");
-// console.log(liNodes);
-// function Amendments(elementNodes, searchText) {
-//   this.textNodes = elementNodes;
-//   this.searchText = searchText.toLowerCase();
-// }
-//
-// Amendments.prototype.filterAmendments = function(){
-//   let regex = new RegExp(this.searchText);
-//
-//   let list = document.getElementsByClassName('amendments');
-//   console.log(list);
-//   let result = [];
-//
-//   for(let i=0; i < this.textNodes.length; i++){
-//     let counter = i+1;
-//     let textNode = this.textNodes[i].innerHTML;
-//     let cleanTextNode = textNode.replace(/;/g, " ").toLowerCase();
-//
-//     if(regex.test(cleanTextNode)){
-//        result.push('<span class="number">' + counter +'</span>' +
-//        '<ul class="list-group"><li class="list-group-item">'+ textNode + "</li></ul>");
-//     }
-//   }
-//   list[0].innerHTML = result;
-// }
-//
-// // If there is something to return start the eventListener
-// window.onload = function runCode(){
-//   if (inputText !== null) {
-//     // Dynamically update the DOM or console every time user enters a key
-//     inputText.addEventListener('keyup', function() {
-//       // save value that user types in search box
-//       var search = inputText.value;
-//       // create a new instance of the amendments constructor
-//       var filteredAmendments = new Amendments(liNodes, search);
-//        filteredAmendments.filterAmendments();
-//     }, false);
-//   }
-// }
-//
-// // Make the message above navbar fades out after it appears
-// $(document).ready(function(){
-//
-//       if($(".error")){
-//         $(".error").delay(3000).slideUp();
-//       }
-//       if($(".message")){
-//         $(".message").delay(3000).slideUp();
-//       }
-//       if($(".notice")){
-//         $(".notice").delay(3000).slideUp();
-//       }
-//       if($(".alert")){
-//         $(".alert").delay(3000).slideUp();
-//       }
-// });
-var liNodes = document.querySelectorAll('ol.amendments li');
+var liNodes = document.querySelectorAll('.amendments ul.list-group li');
 var inputText = document.getElementById("searchBox");
-
-
+console.log(liNodes);
 function Amendments(elementNodes, searchText) {
   this.textNodes = elementNodes;
   this.searchText = searchText.toLowerCase();
@@ -69,21 +10,23 @@ Amendments.prototype.filterAmendments = function(){
   let regex = new RegExp(this.searchText);
 
   let list = document.getElementsByClassName('amendments');
-
+  console.log(list);
   let result = [];
 
   for(let i=0; i < this.textNodes.length; i++){
+    let counter = i+1;
     let textNode = this.textNodes[i].innerHTML;
     let cleanTextNode = textNode.replace(/;/g, " ").toLowerCase();
 
     if(regex.test(cleanTextNode)){
-       result.push("<li>"+ textNode + "</li>");
+       result.push('<span class="number">' + counter +'</span>' +
+       '<ul class="list-group"><li class="list-group-item">'+ textNode + "</li></ul>");
     }
   }
   list[0].innerHTML = result;
 }
 
-// if there is something to return start the eventListener
+// If there is something to return start the eventListener
 window.onload = function runCode(){
   if (inputText !== null) {
     // Dynamically update the DOM or console every time user enters a key
@@ -96,4 +39,20 @@ window.onload = function runCode(){
     }, false);
   }
 }
-// Contact GitHub API Training Shop Blog About
+
+// Make the message above navbar fades out after it appears
+$(document).ready(function(){
+
+      if($(".error")){
+        $(".error").delay(3000).slideUp();
+      }
+      if($(".message")){
+        $(".message").delay(3000).slideUp();
+      }
+      if($(".notice")){
+        $(".notice").delay(3000).slideUp();
+      }
+      if($(".alert")){
+        $(".alert").delay(3000).slideUp();
+      }
+});
