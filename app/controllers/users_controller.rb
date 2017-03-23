0695e685a3fbf8b117d725567620dbb9f119a603
lenn hypolite <lennhydesign@gmail.com>
2017-03-23 03:57:56 -0400

    update code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   # -- show users page of question posted by id
   get "/users/:id" do
     if logged_in?
-      @user = User.find(params[:id])
+      @user = User.find_by_id(params[:id])
       erb :'users/show'
 
     else
