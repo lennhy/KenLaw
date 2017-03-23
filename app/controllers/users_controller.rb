5d221985d5849dcc50c25318252b0f72360757a3
lenn hypolite <lennhydesign@gmail.com>
2017-03-23 05:12:15 -0400

    Test amazon affiliate links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,17 +52,6 @@ class UsersController < ApplicationController
   end
 
 
-  # -- show users page of question posted by id
-  get "/users/:id" do
-    if logged_in?
-      @user = User.find_by_id(params[:id])
-      erb :'users/show'
-
-    else
-      redirect to '/login'
-    end
-  end
-
   # --show current_user profile
   get "/users/profile" do
     if logged_in?
@@ -72,6 +61,15 @@ class UsersController < ApplicationController
     end
   end
 
+  # -- show users page of question posted by id
+  get "/users/:id" do
+    if logged_in?
+      @user = User.find_by_id(params[:id])
+      erb :'users/show'
+    else
+      redirect to "/login"
+    end
+  end
 
   get "/logout" do
     if logged_in?
