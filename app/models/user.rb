479ec63dbf28b17eec97273dec199f03b352eef7
lenn hypolite <lennhydesign@gmail.com>
2017-03-27 13:25:50 -0400

    Changed menu button

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   has_secure_password # --uses bcrypt-ruby automatically handles the storage and generation of salts for you
   validates :username, :email, uniqueness: true
+  validates :amendments, uniqueness: true
   validates :username, :email, presence: true
   validate  :email, :email_format
 
